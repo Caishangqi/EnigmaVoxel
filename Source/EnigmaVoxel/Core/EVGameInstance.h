8e9f1480339e5ad331986427735a952e2b5adecb
CaIzii <2470159730@qq.com>
2025-02-24 02:53:15 -0600

    Initial commit

---
+++ Source/EnigmaVoxel/Core/EVGameInstance.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "EVGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENIGMAVOXEL_API UEVGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+};
---
+++ Source/EnigmaVoxel/EnigmaVoxel.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "EnigmaVoxel.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EnigmaVoxel, "EnigmaVoxel" );
+
+DEFINE_LOG_CATEGORY(LogEnigmaVoxel)
+ 
