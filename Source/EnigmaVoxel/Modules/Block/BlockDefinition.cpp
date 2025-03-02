e955808448d2aaa6b0ce405844c220a1a12ea36c
CaIzii <2470159730@qq.com>
2025-03-02 15:26:28 -0600

    BlockDefinition: rename UBlock to UBlockDefinition for better concept design

---
+++ Source/EnigmaVoxel/Modules/Block/BlockDefinition.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlockDefinition.h"
+
+UBlockDefinition::UBlockDefinition()
+{
+	Model = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Model"));
+}
---
+++ Source/EnigmaVoxel/Modules/Block/BlockDefinition.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnigmaVoxel/Core/Register/Definition.h"
+#include "UObject/Object.h"
+#include "BlockDefinition.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable,BlueprintType)
+class ENIGMAVOXEL_API UBlockDefinition : public UDefinition
+{
+	GENERATED_BODY()
+
+public:
+	UBlockDefinition();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Block")
+	TObjectPtr<UStaticMeshComponent> Model;
+};
