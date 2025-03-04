168aa6f08879b1dd89a6e4afb656881290242beb
CaIzii <2470159730@qq.com>
2025-03-04 02:11:46 -0600

    Block: add MACRO for struct that lets Unreal engine reflect-system to handle it

--- Source/EnigmaVoxel/Modules/Block/Block.h
+++ Source/EnigmaVoxel/Modules/Block/Block.h
@@ -2,17 +2,25 @@
 
 #include "CoreMinimal.h"
 #include "BlockDefinition.h"
+#include "Block.generated.h"
 
 // 一个简单的结构体，用于表示区块中单个方块实例
+USTRUCT(BlueprintType)
 struct FBlock
 {
+	GENERATED_BODY()
+
+public:
 	// 指向全局注册的方块定义
-	UBlockDefinition* Definition = nullptr;
+	UPROPERTY(BlueprintReadWrite)
+	TObjectPtr<UBlockDefinition> Definition = nullptr;
 
 	// 方块在世界或区块中的坐标（例如使用整数坐标）
-	FIntVector Coordinates;
+	UPROPERTY(BlueprintReadWrite)
+	FIntVector Coordinates = FIntVector(0, 0, 0);
 
 	// 例如方块的当前健康值
+	UPROPERTY(BlueprintReadWrite)
 	int32 Health = 100;
 
 	FBlock()
