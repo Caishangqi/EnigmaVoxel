82734aba36fbd9c8ed1de390b058a70d7e494cd9
CaIzii <2470159730@qq.com>
2025-03-04 18:52:23 -0600

    EVGameInstance: use static Delegate to receive the initialized event from RegistrationSubsystem

--- Source/EnigmaVoxel/Core/EVGameInstance.cpp
+++ Source/EnigmaVoxel/Core/EVGameInstance.cpp
@@ -2,3 +2,33 @@
 
 
 #include "EVGameInstance.h"
+
+#include "Register/RegistrationDelegates.h"
+
+TObjectPtr<UEVGameInstance> UEVGameInstance::GameInstance = nullptr;
+
+void UEVGameInstance::Init()
+{
+	GameInstance = this;
+	UE_LOG(LogTemp, Log, TEXT("UEVGameInstance initialized"));
+	FRegistrationDelegates::OnRegistrationInitialize.AddLambda([this]()
+	{
+		GameInstance->OnRegistrationInitialize();
+	});
+	Super::Init();
+}
+
+FGameInstancePIEResult UEVGameInstance::InitializeForPlayInEditor(int32 PIEInstanceIndex, const FGameInstancePIEParameters& Params)
+{
+	return Super::InitializeForPlayInEditor(PIEInstanceIndex, Params);
+}
+
+void UEVGameInstance::OnRegistrationInitialize_Implementation()
+{
+}
+
+
+UEVGameInstance* UEVGameInstance::GET_INSTANCE()
+{
+	return GameInstance;
+}
--- Source/EnigmaVoxel/Core/EVGameInstance.h
+++ Source/EnigmaVoxel/Core/EVGameInstance.h
@@ -6,11 +6,27 @@
 #include "Engine/GameInstance.h"
 #include "EVGameInstance.generated.h"
 
+class UGameInstanceEventDispatcher;
 /**
  * 
  */
-UCLASS()
+UCLASS(Blueprintable)
 class ENIGMAVOXEL_API UEVGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	virtual void                   Init() override;
+	virtual FGameInstancePIEResult InitializeForPlayInEditor(int32 PIEInstanceIndex, const FGameInstancePIEParameters& Params) override;
+
+public:
+	UFUNCTION(BlueprintNativeEvent)
+	void OnRegistrationInitialize();
+	
+private:
+	static TObjectPtr<UEVGameInstance> GameInstance;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static UEVGameInstance* GET_INSTANCE();
 };
