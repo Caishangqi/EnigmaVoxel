0d542e8be79c59f44e312db3ffcc0ae342626e09
CaIzii <2470159730@qq.com>
2025-03-04 18:50:53 -0600

    RegistrationDelegates: introduce RegistrationDelegates that for static global event design

---
+++ Source/EnigmaVoxel/Core/Register/RegistrationDelegates.h
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+DECLARE_MULTICAST_DELEGATE(FOnRegistrationInitialize);
+
+struct ENIGMAVOXEL_API FRegistrationDelegates
+{
+	FRegistrationDelegates() = delete;
+	static FOnRegistrationInitialize OnRegistrationInitialize;
+
+	// static TMulticastDelegate<void(FString)> OnBlockRegistered;
+};
