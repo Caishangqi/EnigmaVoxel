cc11cfea29a3780aca5400887fca804c95186822
CaIzii <2470159730@qq.com>
2025-03-07 09:55:05 -0600

    EnigmaWorldSubsystem: introduce the world subsystem to create/load/unload world.

---
+++ Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnigmaWorldSubsystem.h"
+
+#include "EnigmaVoxel/EnigmaVoxel.h"
+#include "EnigmaVoxel/Core/Log/DefinedLog.h"
+
+void UEnigmaWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	UE_LOG(LogEnigmaVoxelWorld, Warning, TEXT("EnigmaWorldSubsystem::Initialize"));
+	Super::Initialize(Collection);
+}
---
+++ Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "EnigmaWorldSubsystem.generated.h"
+
+class UEnigmaWorld;
+/**
+ * 
+ */
+UCLASS()
+class ENIGMAVOXEL_API UEnigmaWorldSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="World")
+	TMap<int32, TObjectPtr<UEnigmaWorld>> LoadedWorlds;
+
+private:
+};
