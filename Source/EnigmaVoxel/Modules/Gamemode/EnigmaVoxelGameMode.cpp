d8fac91727e54b0a1d50d27aed29cb8791aeaad0
CaIzii <2470159730@qq.com>
2025-03-07 09:52:43 -0600

    EnigmaVoxelGameMode: move the GameMode Class into different folder

--- Source/EnigmaVoxel/Modules/Gamemode/EnigmaVoxelGameMode.cpp
+++ Source/EnigmaVoxel/Modules/Gamemode/EnigmaVoxelGameMode.cpp
@@ -1,8 +1,8 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "EnigmaVoxelGameMode.h"
-#include "EnigmaVoxelPlayerController.h"
-#include "EnigmaVoxelCharacter.h"
+
+#include "EnigmaVoxel/EnigmaVoxelPlayerController.h"
 #include "UObject/ConstructorHelpers.h"
 
 AEnigmaVoxelGameMode::AEnigmaVoxelGameMode()
