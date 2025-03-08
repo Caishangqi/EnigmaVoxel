84e9871452130b127d8b9bbde32f8f76722745c3
CaIzii <2470159730@qq.com>
2025-03-07 23:48:44 -0600

    EnigmaWorldSubsystem: define the World subsystem that supporting Chunk streaming and Player Join

--- Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.cpp
+++ Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.cpp
@@ -3,11 +3,60 @@
 
 #include "EnigmaWorldSubsystem.h"
 
+#include "EnigmaWorld.h"
+#include "EnigmaWorldDelegates.h"
 #include "EnigmaVoxel/EnigmaVoxel.h"
 #include "EnigmaVoxel/Core/Log/DefinedLog.h"
 
 void UEnigmaWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
 {
-	UE_LOG(LogEnigmaVoxelWorld, Warning, TEXT("EnigmaWorldSubsystem::Initialize"));
+	UE_LOG(LogEnigmaVoxelWorld, Display, TEXT("EnigmaWorldSubsystem::Initialize"));
+	/// Development Only
+	TObjectPtr<UWorld> currentUWorld = GetWorld();
+	if (currentUWorld == nullptr)
+	{
+		UE_LOG(LogEnigmaVoxelWorld, Error, TEXT("Current world is NULL"));
+	}
+	else
+	{
+		// We let EnigmaWorld delegate the UWorld
+		CreateEnigmaWorld(0)->SetUWorldTarget(currentUWorld);
+	}
+
+	// Time Handle
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &UEnigmaWorldSubsystem::UpdateWorldTick, 0.05f, true);
+
+	// Event Binding
+	FEnigmaWorldDelegates::OnPlayerJoinWorld.AddUObject(this, &UEnigmaWorldSubsystem::OnPlayerJoinWorld);
+
+	/// 
 	Super::Initialize(Collection);
+}
+
+UEnigmaWorld* UEnigmaWorldSubsystem::CreateEnigmaWorld(int32 WorldID)
+{
+	UE_LOG(LogEnigmaVoxelWorld, Display, TEXT("Creating Enigma World, ID -> %d"), WorldID);
+	UEnigmaWorld* result = NewObject<UEnigmaWorld>(this);
+	LoadedWorlds.Add(WorldID, result);
+	return result;
+}
+
+void UEnigmaWorldSubsystem::UpdateWorldTick()
+{
+	for (TTuple<int, TObjectPtr<UEnigmaWorld>> LoadedWorld : LoadedWorlds)
+	{
+		if (LoadedWorld.Value->GetEnableWorldTick())
+		{
+			LoadedWorld.Value->UpdateStreamingChunks();
+		}
+	}
+}
+
+void UEnigmaWorldSubsystem::OnPlayerJoinWorld(int32 WorldIndex, AActor* Player)
+{
+	if (LoadedWorlds[WorldIndex])
+	{
+		LoadedWorlds[WorldIndex]->AddEntity(Cast<APawn>(Player));
+		UE_LOG(LogEnigmaVoxelWorld, Display, TEXT("Player %s Join the World %d"), *Player->GetName(), WorldIndex);
+	}
 }
--- Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.h
+++ Source/EnigmaVoxel/Core/World/EnigmaWorldSubsystem.h
@@ -17,10 +17,21 @@ class ENIGMAVOXEL_API UEnigmaWorldSubsystem : public UGameInstanceSubsystem
 
 public:
 	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
-	
+
+protected:
+	UFUNCTION(BlueprintCallable, Category="World")
+	UEnigmaWorld* CreateEnigmaWorld(int32 WorldID);
+	UFUNCTION(BlueprintCallable, Category="World")
+	void UpdateWorldTick();
+
 public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="World")
 	TMap<int32, TObjectPtr<UEnigmaWorld>> LoadedWorlds;
 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="Chunk")
+	FTimerHandle TimerHandle;
+
+public:
+	void OnPlayerJoinWorld(int32 WorldIndex, AActor * Player);
 private:
 };
