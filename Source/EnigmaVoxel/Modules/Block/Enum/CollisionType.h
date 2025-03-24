545ec2c33b3fd7fef694b2a410f1c6d72de59b0b
CaIzii <2470159730@qq.com>
2025-03-24 02:49:35 -0500

    CollisionType: split the definition of ECollisionType into a separate header file.

---
+++ Source/EnigmaVoxel/Modules/Block/Enum/CollisionType.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+
+UENUM(BlueprintType)
+enum class ECollisionType: uint8
+{
+	UNIT_BLOCK,
+	UNIT_BLOCK_HALF,
+	CUSTOM
+};
