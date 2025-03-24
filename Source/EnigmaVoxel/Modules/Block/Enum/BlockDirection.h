2e148a2989b8eb135e20ff7148a476782177d49f
CaIzii <2470159730@qq.com>
2025-03-24 02:48:33 -0500

    BlockDirection: split the definition into a separate header file to fix circular dependency.

---
+++ Source/EnigmaVoxel/Modules/Block/Enum/BlockDirection.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+UENUM(BlueprintType)
+enum class EBlockDirection: uint8
+{
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
+	UP,
+	DOWN
+};
