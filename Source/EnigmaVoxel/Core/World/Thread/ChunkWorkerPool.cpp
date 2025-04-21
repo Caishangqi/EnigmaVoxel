21e988aff99122e393cd82add9485fcbc3826703
CaIzii <2470159730@qq.com>
2025-04-21 03:20:29 -0500

    ChunkWorker: Introduce ChunkWorker that hold user-specific jobs (TFunctions).

---
+++ Source/EnigmaVoxel/Core/World/Thread/ChunkWorkerPool.cpp
@@ -0,0 +1,76 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ChunkWorkerPool.h"
+
+#include "ChunkWorker.h"
+#include "EnigmaVoxel/Core/Log/DefinedLog.h"
+
+UChunkWorkerPool::UChunkWorkerPool()
+{
+}
+
+bool UChunkWorkerPool::Init(int32 NumThreads)
+{
+	UE_LOG(LogEnigmaVoxelChunk, Warning, TEXT("UChunkWorkerPool::Init Preparing Create UChunkWorkerPool with %d workers"), NumThreads);
+	if (NumThreads <= 0)
+	{
+		NumThreads = FMath::Max(1, FPlatformMisc::NumberOfCores());
+	}
+
+	for (int32 i = 0; i < NumThreads; ++i)
+	{
+		FString          ThreadName = FString(TEXT("Chunk Worker Thread: %d"), i);
+		auto*            Worker     = new FChunkWorker(IdleCounter, i);
+		FRunnableThread* Thread     = FRunnableThread::Create(Worker, *ThreadName, 0, TPri_Normal);
+		if (!Thread)
+		{
+			return false;
+		}
+
+		Workers.Add(Worker);
+		Threads.Add(Thread);
+	}
+	UE_LOG(LogEnigmaVoxelChunk, Warning, TEXT("UChunkWorkerPool::Init Created with %d threads and %d workers"), NumThreads, Workers.Num());
+	return true;
+}
+
+void UChunkWorkerPool::RequestExit()
+{
+	bStopping = true;
+	for (FChunkWorker* W : Workers)
+	{
+		if (W)
+		{
+			W->Stop();
+		}
+	}
+}
+
+void UChunkWorkerPool::Shutdown()
+{
+	for (int32 i = 0; i < Threads.Num(); ++i)
+	{
+		if (Threads[i])
+		{
+			Threads[i]->WaitForCompletion(); // Halts the caller
+			delete Threads[i];
+		}
+		delete Workers[i];
+	}
+	Threads.Empty();
+	Workers.Empty();
+	UE_LOG(LogEnigmaVoxelChunk, Warning, TEXT("UChunkWorkerPool::Shutdown Shutting down and release workers."))
+}
+
+FChunkWorker* UChunkWorkerPool::GetIdleWorker()
+{
+	for (FChunkWorker* W : Workers)
+	{
+		if (W && W->IsIdle())
+		{
+			return W;
+		}
+	}
+	return nullptr;
+}
---
+++ Source/EnigmaVoxel/Core/World/Thread/ChunkWorkerPool.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ChunkWorkerPool.generated.h"
+
+class FChunkWorker;
+/**
+ * 
+ */
+UCLASS()
+class ENIGMAVOXEL_API UChunkWorkerPool : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UChunkWorkerPool();
+
+	bool Init(int32 NumThreads);
+	void RequestExit();
+	void Shutdown();
+
+	// Returns an idle Worker; if there is no idle Worker, returns nullptr
+	FChunkWorker* GetIdleWorker();
+
+	/** Statistics interface */
+	int32 GetNumThreads() const { return Workers.Num(); }
+	int32 GetNumIdleThreads() const { return IdleCounter.GetValue(); }
+
+private:
+	TArray<FChunkWorker*>    Workers; // One-to-one correspondence with Threads index
+	TArray<FRunnableThread*> Threads;
+
+	TAtomic<bool> bStopping{false};
+
+	FThreadSafeCounter IdleCounter;
+};
