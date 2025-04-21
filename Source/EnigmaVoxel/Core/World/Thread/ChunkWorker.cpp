6f9c0fb9ada0af0578a5a7a9e34cef3c5f4ff755
CaIzii <2470159730@qq.com>
2025-04-21 03:21:45 -0500

    ChunkWorker: Introduce ChunkWorker which holds lambda func and handles thread automatic operation.

---
+++ Source/EnigmaVoxel/Core/World/Thread/ChunkWorker.cpp
@@ -0,0 +1,53 @@
+﻿#include "ChunkWorker.h"
+
+#include "EnigmaVoxel/Core/Log/DefinedLog.h"
+
+FChunkWorker::FChunkWorker(FThreadSafeCounter& InIdleCounter, int InId)
+	: Id(InId), IdleCounter(InIdleCounter)
+{
+	WorkEvent = FPlatformProcess::GetSynchEventFromPool(false);
+	UE_LOG(LogEnigmaVoxelWorker, Warning, TEXT("FChunkWorker [%d] is created"), Id);
+}
+
+FChunkWorker::~FChunkWorker()
+{
+	FPlatformProcess::ReturnSynchEventToPool(WorkEvent);
+	WorkEvent = nullptr;
+	UE_LOG(LogEnigmaVoxelWorker, Warning, TEXT("FChunkWorker [%d] was deleted"), Id);
+}
+
+void FChunkWorker::AssignJob(TFunction<void()>&& InJob)
+{
+	FScopeLock _(&JobMutex);
+	Job   = MoveTemp(InJob);
+	bIdle = false;
+	IdleCounter.Decrement();
+	WorkEvent->Trigger();
+}
+
+uint32 FChunkWorker::Run()
+{
+	while (!bStop)
+	{
+		WorkEvent->Wait();
+		if (bStop)
+		{
+			break;
+		}
+
+		TFunction<void()> LocalJob;
+		{
+			FScopeLock _(&JobMutex);
+			LocalJob = MoveTemp(Job);
+		}
+		if (LocalJob)
+		{
+			LocalJob();
+		}
+
+		// 还原空闲状态
+		bIdle = true;
+		IdleCounter.Increment();
+	}
+	return 0;
+}
---
+++ Source/EnigmaVoxel/Core/World/Thread/ChunkWorker.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+class FChunkWorker : public FRunnable
+{
+public:
+	explicit FChunkWorker(FThreadSafeCounter& InIdleCounter, int InId);
+	virtual  ~FChunkWorker() override;
+
+	void AssignJob(TFunction<void()>&& InJob);
+	bool IsIdle() const { return bIdle; }
+
+	virtual uint32 Run() override;
+
+	virtual void Stop() override
+	{
+		bStop = true;
+		WorkEvent->Trigger();
+	}
+
+private:
+	TFunction<void()> Job;
+	FCriticalSection  JobMutex;
+
+	int Id = 0;
+
+	FThreadSafeBool bIdle{true};
+	FThreadSafeBool bStop{false};
+
+	FEvent*             WorkEvent = nullptr;
+	FThreadSafeCounter& IdleCounter;
+};
