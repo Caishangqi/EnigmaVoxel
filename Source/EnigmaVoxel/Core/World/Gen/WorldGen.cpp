ba976e25ebd4ad9475f13a81a406b3640826d15d
CaIzii <2470159730@qq.com>
2025-05-16 17:43:35 -0500

    WorldGen: Introduce `WorldGen` struct that encapsulates Gene func.

---
+++ Source/EnigmaVoxel/Core/World/Gen/WorldGen.cpp
@@ -0,0 +1,51 @@
+﻿#include "WorldGen.hpp"
+
+#include "EnigmaVoxel/Modules/Block/Block.h"
+#include "EnigmaVoxel/Modules/Chunk/ChunkHolder.h"
+
+void FWorldGen::GenerateFullChunk(FChunkHolder& H)
+{
+	//H.RefreshMaterialCache();
+	H.FillChunkWithArea(FIntVector(16, 16, 8), "Enigma", "Blue Enigma Block");
+	UE::Geometry::FDynamicMesh3 Tmp;
+	for (int z = 0; z < H.Dimension.Z; ++z)
+	{
+		for (int y = 0; y < H.Dimension.Y; ++y)
+		{
+			for (int x = 0; x < H.Dimension.X; ++x)
+			{
+				const FBlock& B = H.GetBlock({x, y, z});
+				if (!B.Definition)
+				{
+					continue;
+				}
+				AppendBoxForBlock(Tmp, B, H);
+			}
+		}
+	}
+	H.Mesh = MoveTemp(Tmp);
+}
+
+void FWorldGen::RebuildMesh(FChunkHolder& H)
+{
+	H.Mesh = UE::Geometry::FDynamicMesh3();
+	UE::Geometry::FDynamicMesh3 Tmp;
+	H.RefreshMaterialCache();
+	for (int z = 0; z < H.Dimension.Z; ++z)
+	{
+		for (int y = 0; y < H.Dimension.Y; ++y)
+		{
+			for (int x = 0; x < H.Dimension.X; ++x)
+			{
+				const FBlock& B = H.GetBlock({x, y, z});
+				if (!B.Definition)
+				{
+					continue;
+				}
+				AppendBoxForBlock(Tmp, B, H);
+			}
+		}
+	}
+	H.Mesh = MoveTemp(Tmp);
+	H.bDirty.store(false);
+}
---
+++ Source/EnigmaVoxel/Core/World/Gen/WorldGen.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+struct FChunkHolder;
+
+struct FWorldGen
+{
+	static void GenerateFullChunk(FChunkHolder& H);
+	static void RebuildMesh(FChunkHolder& H);
+};
